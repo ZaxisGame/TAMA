3636bbb1915d7dd64e5736fccf71e5afc95fa8f0
okidoKishie <39988384+okidoKishie@users.noreply.github.com>
2018-08-12 20:22:13 +0900

    敵の生死判定の修正

--- TAMA/Assets/Motohoshi/scripts/PlayerManager_M.cs
+++ TAMA/Assets/Motohoshi/scripts/PlayerManager_M.cs
@@ -4,50 +4,55 @@ using UnityEngine;
 
 public class PlayerManager_M: MonoBehaviour {
 
-
+    //ゲームマネージャー取得
+    private GameObject gamemanager;
+    GameManager_scr_K Game_M;
+    //ライフマネージャー取得
+    private GameObject canbas;
     LifeManager_scr_K Life_M;
-    private GameObject gm;
+    EnemyController_scr_K enemyController;
+    void Start()
+    {
+        //ゲームマネージャー取得
+        gamemanager = GameObject.Find("GameManager");
+        Game_M = gamemanager.GetComponent<GameManager_scr_K>();
+        //ライフマネージャー取得
+        canbas = GameObject.Find("Canvas");
+        Life_M = canbas.GetComponent<LifeManager_scr_K>();
 
-    void Start () {
-        gm = GameObject.Find("Canvas");
-        Life_M = gm.GetComponent<LifeManager_scr_K>();
-        
     }
-    
-    // Update is called once per frame
-    void Update () {
-        
+
+    void Update()
+    {
+
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        //当たった敵は生きている
-        EnemyController_scr_K.isAlive = true;
 
-        //ジャンプ中に頭をふむと
-        if (col.CompareTag("EnemyTop") && PlayerMove_scr_K.isJump && EnemyController_scr_K.isAlive == true)
+        // ジャンプ中に頭をふむと
+        if (col.CompareTag("EnemyTop") && PlayerMove_scr_K.isJump)
         {
+            //当たった敵のスクリプトを取得
+            enemyController = col.gameObject.GetComponent<EnemyController_scr_K>();
+            //プレイヤーにダメージが入らないようにする
+            enemyController.isAlive = false;
+
             Debug.Log("敵を倒した！");
-            //敵を消去。プレイヤーにダメージが入らないようにする
-            EnemyController_scr_K.isAlive = false;
 
+            //ジャンプする
             gameObject.GetComponent<PlayerMove_scr_K>().Jump();
-
-
+            //敵を消去
             Destroy(col.gameObject.transform.parent.gameObject);
-
         }
 
         //敵に当たると
-        else if (col.CompareTag("Enemy") && EnemyController_scr_K.isAlive == true )
+        else if (col.CompareTag("Enemy") && col.gameObject.GetComponent<EnemyController_scr_K>().isAlive)
         {
-            Debug.Log("ダメージ！");
-            Life_M.Damage();
 
+            Life_M.Damage();
             Destroy(col.gameObject);
-           
-            //プレイヤーにダメージ判定
-            // LifeManager_M.Damage();的な
+
         }
     }
 }
