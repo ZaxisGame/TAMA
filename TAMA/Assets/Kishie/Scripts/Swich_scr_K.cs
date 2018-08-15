2059d231008684a3b595c54ea59a55172489c91d
okidoKishie <39988384+okidoKishie@users.noreply.github.com>
2018-08-16 00:47:59 +0900

    fbx追加、スイッチでインスタンス生成機能追加

---
+++ TAMA/Assets/Kishie/Scripts/Swich_scr_K.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swich_scr_K : MonoBehaviour {
+
+    public GameObject enemys;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            GameObject EnemysInstance = Instantiate(enemys,enemys.transform.position, Quaternion.identity);
+        }
+
+		
+	}
+}
